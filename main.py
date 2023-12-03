90c39f9a9022f78b3d10ec5f11d3411d8d0793b5
Marelso <klein.marcelojr@gmail.com>
2023-12-03 10:11:05 -0300

    Including method to handle time of day check

--- main.py
+++ main.py
@@ -10,33 +10,25 @@ def take_screenshot():
     return cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
 
 
-def is_energy_low(energy_level):
+def is_red(screenshot):
     lower_red = np.array([0, 0, 200])
     upper_red = np.array([50, 50, 255])
 
-    return cv2.inRange(energy_level, lower_red, upper_red).any()
+    return cv2.inRange(screenshot, lower_red, upper_red).any()
 
 
 def get_energy_level(screenshot):
-    # Energy bar position
-    start_point = (1876, 890)
-    end_point = (1898, 1053)
+    energy_level = crop_image(screenshot, (1876, 890), (1898, 1053))
 
-    x, y = start_point
-    width = end_point[0] - start_point[0]
-    height = end_point[1] - start_point[1]
-
-    energy_level = screenshot[y:y + height, x:x + width]
-
-    cv2.imwrite(f"energy_level_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png", energy_level)
+    log_screenshot(f"energy_level_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png", energy_level)
 
     return energy_level
 
 
 def get_time_of_day(screenshot):
-    time_of_day = crop_image(screenshot, (1729, 122), (1883, 153))
+    time_of_day = crop_image(screenshot, (1729, 122), (1885, 164))
 
-    cv2.imwrite(f"time_of_day_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png", time_of_day)
+    log_screenshot(f"time_of_day_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png", time_of_day)
 
     return time_of_day
 
@@ -50,21 +42,29 @@ def crop_image(screenshot, starting_position, ending_position):
     return screenshot[y:y + height, x:x + width]
 
 
-def main():
+def log_screenshot(filename, screenshot):
+    cv2.imwrite(filename, screenshot)
+
+
+def check_status():
     screenshot = take_screenshot()
 
     energy_level = get_energy_level(screenshot)
     time_of_day = get_time_of_day(screenshot)
 
-    if is_energy_low(energy_level):
-        print("The specified region is red.")
+    if not (is_red(energy_level) and is_red(time_of_day)):
+        print("enough energy and time.")
+        return True
     else:
-        print("The specified region is not red.")
+        print(f"low energy or not enough time. Is energy enough: {is_red(energy_level)} is time enough: {is_red(time_of_day)}")
+        return False
+
+
+def main():
+    while True:
+        time.sleep(5)
+        check_status()
 
 
 if __name__ == "__main__":
-    time.sleep(5)
     main()
-    while True:
-        time.sleep(5)
-        print(pyautogui.position())
